ebb98fa3cfba2a7e92683b2b95617455e5dd1f3b
Manas <manasranjansahoo971@gmail.com>
2025-04-11 22:13:48 +0530

    Add render URL

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ app.use(cookieParser())
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: 'http://localhost:5173', // frontend URL
+    origin: 'https://agreeverse-app.onrender.com',
     credentials: true
 }));
 
