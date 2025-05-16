f6b1d73c037a9f20c1b6e2cdbd4ed509892c01ff
Manas <manasranjansahoo971@gmail.com>
2025-05-16 12:28:24 +0530

    Update.

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {ProductProvider} from './useContext/productContext.jsx'
+import { ProductProvider } from './useContext/productContext.jsx'
+import { ProductContext } from "./useContext/productContext.jsx";
 import { AuthProvider } from './useContext/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
